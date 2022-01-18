83702078e49a3e3906b6673fb4b9340f09bfd4d2
schiderf <felix.schider02@htl-salzburg.ac.at>
2022-01-18 09:15:13 +0100

    write start script, add scriptable object and start with main scene

---
+++ Assets/MyGame/Scripts/VariablesGlobal.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "MyGame/Scripts/VariablesData", order = 1)]
+public class VariablesGlobal : ScriptableObject
+{
+public string playerNameString;
+
+public int score;
+}
